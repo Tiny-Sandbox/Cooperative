2de89dfdc456d8c0bac2b6f34ac9874f7e442325
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-06-11 15:27:29 -0400

    Bring out CooperativePuzzleWall and CooperativeSwitch from base game

---
+++ index.js
@@ -0,0 +1,36 @@
+const basic = require("tsa-basic").tiles;
+
+class CooperativeSwitch extends basic.Space {
+	constructor(x, y) {
+		super(x, y);
+
+		this.color = "#7777FF";
+	}
+}
+
+class CooperativePuzzleWall extends basic.Wall {
+	constructor(strengthNeeded, x, y) {
+		super(x, y);
+
+		this.color = "#9999FF";
+		this.strengthNeeded = strengthNeeded;
+	}
+
+	collides(dir, pl, arenaMap) {
+		return arenaMap.getMatchingTiles((item) => {
+			if (item.constructor.name !== "Occupied") return false;
+			return item.oldTile.constructor.name === "CooperativeSwitch";
+		}).length < this.strengthNeeded;
+	}
+
+	toString() {
+		return "Cooperative wall";
+	}
+}
+
+module.exports = {
+	tiles: {
+		CooperativeSwitch,
+		CooperativePuzzleWall,
+	},
+};
